7d51e94fa3a47518c10f75ccd4827ad56c90917e
tobes <toby.junk@gmail.com>
2013-05-02 17:46:53 +0100

    slo fixes

--- ckanext/saml2/plugin.py
+++ ckanext/saml2/plugin.py
@@ -257,24 +257,32 @@ class Saml2Controller(base.BaseController):
         # so here I might get either a LogoutResponse or a LogoutRequest
         client = environ['repoze.who.plugins']['saml2auth']
         if 'QUERY_STRING' in environ:
-            saml_resp = p.toolkit.request.GET.get('SAMLResponse')
-            # fix the cert so that it is on multiple lines
-            out = []
-            # if on multiple lines make it a single one
-            line = ''.join(saml_resp.split('\n'))
-            while len(line) > 64:
-                out.append(line[:64])
-                line = line[64:]
-            out.append(line)
-            saml_resp = '\n'.join(out)
-            try:
-                res = client.saml_client.logout_request_response(
-                    saml_resp,
-                    binding=BINDING_HTTP_REDIRECT
-                )
-            except KeyError:
-                # return error reply
-                pass
+            saml_resp = p.toolkit.request.GET.get('SAMLResponse', '')
+            saml_req = p.toolkit.request.GET.get('SAMLRequest', '')
 
-        delete_cookies()
-        h.redirect_to(controller='user', action='logged_out')
+            if saml_req:
+                get = p.toolkit.request.GET
+                subject_id = environ["repoze.who.identity"]['repoze.who.userid']
+                headers, success = client.saml_client.do_http_redirect_logout(get, subject_id)
+                h.redirect_to(headers[0][1])
+            if saml_resp:
+                # fix the cert so that it is on multiple lines
+                out = []
+                # if on multiple lines make it a single one
+                line = ''.join(saml_resp.split('\n'))
+                while len(line) > 64:
+                    out.append(line[:64])
+                    line = line[64:]
+                out.append(line)
+                saml_resp = '\n'.join(out)
+                try:
+                    res = client.saml_client.logout_request_response(
+                        saml_resp,
+                        binding=BINDING_HTTP_REDIRECT
+                    )
+                except KeyError:
+                    # return error reply
+                    pass
+
+                delete_cookies()
+                h.redirect_to(controller='user', action='logged_out')
