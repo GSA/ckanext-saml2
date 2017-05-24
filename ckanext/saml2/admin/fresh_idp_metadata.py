c8b00a2f6d4d31bf8321b4a197625db7395b6388
Artem Bazykin <artem.bazykin@linkdigital.com.au>
2017-05-24 11:57:12 +0300

    SXFNSWXZAI-240 / Added XML file check for not well-formed

--- ckanext/saml2/admin/fresh_idp_metadata.py
+++ ckanext/saml2/admin/fresh_idp_metadata.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 
+import sys
+import os
 import xml.etree.cElementTree as ET
 from datetime import datetime
 import requests
@@ -27,12 +29,22 @@ validUntil = datetime_object.date()
 leftDays = (validUntil - datetime.today().date()).days
 
 if leftDays <= 1:
+    new_filename = '/tmp/' + os.path.basename(args.path)
     try:
         r = requests.get(args.url, stream=True, verify=False)
     except requests.exceptions.RequestException as e:
         print 'Error: {0}'.format(e)
+        sys.exit('Error while try to download new metadata xml file')
     if r.status_code == 200:
-        with open('nswidhub-test-idp-new.xml', 'wb') as f:
+        with open(new_filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024):
                 if chunk:
                     f.write(chunk)
+    f = open(new_filename, 'r')
+    data = f.read()
+    f.close()
+    try:
+        x = ET.fromstring(data)
+    except ET.ParseError as e:
+        print 'ParseError: {0}'.format(e)
+        sys.exit('Error with new metadata xml file')
