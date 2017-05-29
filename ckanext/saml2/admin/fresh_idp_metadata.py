18bdceace85763655021554de4d8ea5f02bffe4a
Mark Gregson <mark.gregson@linkdigital.com.au>
2017-05-29 21:11:12 +1000

    SXFNSWXZAI-240 perform metadata replacement atomically
    Also some more error handling, small refeactor and updated readme.

--- ckanext/saml2/admin/fresh_idp_metadata.py
+++ ckanext/saml2/admin/fresh_idp_metadata.py
@@ -4,9 +4,21 @@ import sys
 import os
 import xml.etree.cElementTree as ET
 from datetime import datetime
-import requests
 from argparse import ArgumentParser
+import requests
 
+NS = {
+    "md": "urn:oasis:names:tc:SAML:2.0:metadata",
+    "dsig": "http://www.w3.org/2000/09/xmldsig#",
+    "enc": "http://www.w3.org/2001/04/xmlenc#",
+    "mdattr": "urn:oasis:names:tc:SAML:metadata:attribute",
+    "mdext": "urn:oasis:names:tc:SAML:metadata:extension",
+    "ns10": "urn:oasis:names:tc:SAML:profiles:v1metadata",
+    "query": "urn:oasis:names:tc:SAML:metadata:ext:query",
+    "saml": "urn:oasis:names:tc:SAML:2.0:assertion",
+    "x500": "urn:oasis:names:tc:SAML:2.0:profiles:attribute:X500",
+    "xsi": "http://www.w3.org/2001/XMLSchema-instance"
+}
 
 parser = ArgumentParser()
 
@@ -23,28 +35,44 @@ args = parser.parse_args()
 
 tree = ET.ElementTree(file=args.path)
 root = tree.getroot().attrib
-validUntil = root['validUntil']
-datetime_object = datetime.strptime(validUntil, '%Y-%m-%dT%H:%M:%SZ')
-validUntil = datetime_object.date()
-leftDays = (validUntil - datetime.today().date()).days
+valid_until = root.get('validUntil', None)
+if not valid_until:
+    sys.exit('Metadata has no expiry date')
 
-if leftDays <= 1:
-    new_filename = '/tmp/' + os.path.basename(args.path)
+root = None
+tree = None
+
+valid_until = datetime.strptime(valid_until, '%Y-%m-%dT%H:%M:%SZ').date()
+days_till_expiry = (valid_until - datetime.utcnow().date()).days
+
+#print '%s days till expiry' % (days_till_expiry)
+
+if days_till_expiry <= 4:
+    tmp_filename = os.path.join(os.path.dirname(args.path), 'tmp-{0}.xml'.format(datetime.now()))
     try:
-        r = requests.get(args.url, stream=True, verify=False)
+        r = requests.get(args.url, allow_redirects=True)
+        r.raise_for_status()
     except requests.exceptions.RequestException as e:
         print 'Error: {0}'.format(e)
-        sys.exit('Error while try to download new metadata xml file')
-    if r.status_code == 200:
-        with open(new_filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024):
-                if chunk:
-                    f.write(chunk)
-    f = open(new_filename, 'r')
-    data = f.read()
-    f.close()
+        sys.exit('Error downloading new metadata XML')
+
     try:
-        x = ET.fromstring(data)
+        metadata = ET.ElementTree(ET.fromstring(r.text))
     except ET.ParseError as e:
         print 'ParseError: {0}'.format(e)
-        sys.exit('Error with new metadata xml file')
+        sys.exit('Error parsing new metadata XML')
+
+    # Remove IdP config for POST binding for SLO, which ckanext-saml2
+    # currently doesn't support, forcing pysaml2 to use the Redirect
+    # binding
+    for idpsso in metadata.findall('./md:IDPSSODescriptor', NS):
+        for element in idpsso.findall('./md:SingleLogoutService[@Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"]', NS):
+            #print ET.tostring(element)
+            idpsso.remove(element)
+            #print "Removed IdP POST binding SLO config"
+
+    metadata.write(tmp_filename, encoding="utf-8")
+
+    # Atomically replace old with new
+    os.rename(tmp_filename, args.path)
+    #print "Replaced metadata"
